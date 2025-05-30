14685b5def59c8aeebd7fbe2730e40d8b9b5682a
Seochangbum <ckdqja1115@naver.com>
2025-05-30 10:40:00 +0900

    가열로, 압연기 페이지 푸시

---
+++ smartFactory_MillProcess/Views/RollingMachinePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace smartFactory_MillProcess.Views
+{
+    /// <summary>
+    /// RollingMachinePage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class RollingMachinePage : Page
+    {
+        public RollingMachinePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
