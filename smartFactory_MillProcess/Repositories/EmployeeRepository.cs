9e85bd8599196ccda4122b6082fad699e0ccf8bc
HyunBaeL <seehb126@naver.com>
2025-05-30 11:53:02 +0900

    back & logout & 사원관리 DB 로딩

---
+++ smartFactory_MillProcess/Repositories/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using smartFactory_MillProcess.Models;
+
+namespace smartFactory_MillProcess.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly string myConnection = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+
+        public async Task<List<Employee>> GetEmployee()
+        {
+            List<Employee> emp = new List<Employee>();
+            
+            using var conn = new MySqlConnection(myConnection);
+            await conn.OpenAsync();
+
+            string sql = "SELECT * FROM employee";
+            using var cmd = new MySqlCommand(sql, conn);
+            using var reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                emp.Add(new Employee()
+                {
+                    EmployeeId = reader.GetInt32(reader.GetOrdinal("employee_id")),
+                    EmployeeCode = reader.GetString(reader.GetOrdinal("employee_code")),
+                    Position = reader.GetString(reader.GetOrdinal("position")),
+                    Department = reader.GetString(reader.GetOrdinal("department")),
+                    HireDate = reader.GetDateTime(reader.GetOrdinal("hire_date")),
+                    EmployeeStatus = reader.GetString(reader.GetOrdinal("employee_status")),
+                    SafetyEducation = reader.GetBoolean(reader.GetOrdinal("safety_Education"))
+                });
+            }
+
+            return emp;
+        } 
+    }
+}
---
+++ smartFactory_MillProcess/Views/EmployeeManagementPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using smartFactory_MillProcess.ViewModels;
+
+namespace smartFactory_MillProcess.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeeManagementPage.xaml
+    /// </summary>
+    public partial class EmployeeManagementPage : Page
+    {
+        public EmployeeManagementPage()
+        {
+            InitializeComponent();
+            DataContext = new EmployeeViewModel();
+        }
+    }
+}
